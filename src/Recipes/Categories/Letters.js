a7d6bccb2c7702767c68f4c68e03f1bf45a4616b
gema <gjcuenca@hotmail.com>
2022-12-09 21:37:46 +0100

    api

--- src/Recipes/Categories/Letters.js
+++ src/Recipes/Categories/Letters.js
@@ -1,4 +1,5 @@
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
+import * as API from '../../Services/launchesApis';
 import './Letters.css'
 import { useState, useEffect } from 'react'
 import axios from 'axios'
@@ -9,26 +10,26 @@ const Letters = () => {
   const [click, setClick] = useState(false)
   let array = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 
-
-    useEffect(() => {
-        const getData = async () => {
-            const resp = await axios.get('https://www.themealdb.com/api/json/v1/1/search.php?f=b')
-            setClick(resp.data.meals);
-        }
-        getData();
-    }, []);
+  const [launches,setLaunches] = useState([])
+  
+    useEffect(()=>{
+        API.getLauncheByLetter('b').then (setLaunches);
+    },[click]);
+    
+  
+  
 
   return (
     <div>
       <div className='letters'>
         {array.map((item) => 
-              <button onClick={()=>{setClick(!click)} } className='clickable'>
+              <button onClick={()=>{setClick(!click)} } className='clickable' key={item}>
                     {item}
               </button>
             )}    
       </div>
       <div className='meals2'>
-          {click? click.map((key) => <MediaCard 
+          {click? launches.map((key) => <MediaCard 
           className="meal" 
           key={key.idMeal} 
           img={key.strMealThumb} 
