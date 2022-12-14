ecbb507105504e503e51d951ea4eedfc1e72ac66
gema <gjcuenca@hotmail.com>
2022-12-14 10:31:19 +0100

    warning

--- src/Home/FeaturedRecipe.jsx
+++ src/Home/FeaturedRecipe.jsx
@@ -1,10 +1,14 @@
-
 import "./FeaturedRecipe.css";
 
 export default function FeaturedRecipe(props) {
     return (
         <div className="recipe_container">
-
+            <div>
+                <img component="img" className='recipe_img'
+                    src={props.img}
+                    alt={props.meal}
+                />
+            </div>
             <div className="recipe_text">
                 <div className="recipe_title">
                     <h2 className="recipe_section">Featured Recipe </h2>
@@ -14,12 +18,6 @@ export default function FeaturedRecipe(props) {
                     <p>{props.description}</p>
                 </div>
                 <button className='btn'>Detail</button>
-            </div>  
-            <div>
-                <img component="img" className='recipe_img'
-                    src={props.img}
-                    alt={props.meal}
-                />
             </div>
         </div>
     );
