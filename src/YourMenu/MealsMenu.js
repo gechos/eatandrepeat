9a1200439388046cc2f6fdc790b99132f958da8c
laurapellicerrahan <laura.pellicer.rahan@gmail.com>
2022-12-19 22:10:08 +0100

    Last Changes

--- src/YourMenu/MealsMenu.js
+++ src/YourMenu/MealsMenu.js
@@ -1,41 +1,42 @@
 import CardMedia from '@mui/material/CardMedia';
 import './Heart.css';
-import { FaHeart} from 'react-icons/fa';
-import {Link} from 'react-router-dom';
+import { FaHeart } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+import './cardMenu.css'
 
-const MealsMenu = ({recip, setSelect}) => {
-    let menu=[...recip]
-  
+const MealsMenu = ({ recip, setSelect }) => {
+  let menu = [...recip]
+
   //handle the click
   const OnClickHeart = (id) => {
-    console.log(id,menu);
-    menu = menu.filter(menu => menu[0] !== `${id}`);        
+    console.log(id, menu);
+    menu = menu.filter(menu => menu[0] !== `${id}`);
     setSelect(menu);
   }
- 
+
   return (
-      <div className='meals1'>
-          {recip.map((key) => 
-                <div className="cardmeal1">
-                    <div onClick={() => OnClickHeart(key[0])}>
-                    <FaHeart className='heart' />
-                    </div>
-                    <>
-                      <Link to="/CompleteRecipe" state={{id: `${key[0]}`}}>
-                        <CardMedia className="img1" key={key[0]}  
-                                        component="img"
-                                        src={key[2]}
-                                        alt={key[1]} 
-                                    />
-                        <div className="content-text1">
-                              <h4 className="title-meal1">{key[1]}</h4>
-                        </div>
-                      </Link>
-                    </>
-                </div>
-              )}
-        </div> 
-)
+    <div className='meals1'>
+      {recip.map((key) =>
+        <div className="cardmeal1">
+          <>
+            <Link to="/CompleteRecipe" state={{ id: `${key[0]}` }}>
+              <CardMedia className="img1" key={key[0]}
+                component="img"
+                src={key[2]}
+                alt={key[1]}
+              />
+              <div className="content-text1">
+                <h4 className="title-meal1">{key[1]}</h4>
+                <div onClick={() => OnClickHeart(key[0])}>
+                <FaHeart className='heart' />
+              </div>
+              </div>
+            </Link>
+          </>
+        </div>
+      )}
+    </div>
+  )
 }
-      
+
 export default MealsMenu;
--- src/YourMenu/MenuHeader.js
+++ src/YourMenu/MenuHeader.js
@@ -1,51 +1,52 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import MealsMenu from './MealsMenu';
-import { getLauncheByCategory} from '../Services/launchesApis';
+import { getLauncheByCategory } from '../Services/launchesApis';
 import CardMedia from '@mui/material/CardMedia';
-import {Heart} from './Heart';
+import { Heart } from './Heart';
 import './cardMenu.css'
 import HeaderYourMenu from "./HeaderYourMenu";
+import './MenuHeader.css'
 
 
 
 
 const MenuHeader = () => {
 
-  const [categories, setCategories] = useState([]);
-  const [filtered, setFiltered] = useState([]);
-  
-  const [page, setPage] = useState(0);
-  const [pagination, setPagination] = useState([]);
- 
-  const [select, setSelect] = useState([]);
-    
+    const [categories, setCategories] = useState([]);
+    const [filtered, setFiltered] = useState([]);
+
+    const [page, setPage] = useState(0);
+    const [pagination, setPagination] = useState([]);
+
+    const [select, setSelect] = useState([]);
 
 
-  
-  const handleOnClickItem=(item)=>{
-   getLauncheByCategory([item]).then((res)=>{
-      setFiltered(res)
-      let newPagination = [];
-        for (let i = 0; i < filtered.length / 8; i++) {
-            newPagination.push(
-                <button className="button-page" onClick={() => {
-                    setPage(i);
-                }}>{i + 1}</button>);
-        }
-        setPagination(newPagination);
+
+
+    const handleOnClickItem = (item) => {
+        getLauncheByCategory([item]).then((res) => {
+            setFiltered(res)
+            let newPagination = [];
+            for (let i = 0; i < filtered.length / 8; i++) {
+                newPagination.push(
+                    <button className="button-page" onClick={() => {
+                        setPage(i);
+                    }}>{i + 1}</button>);
+            }
+            setPagination(newPagination);
         });
-  }
- 
-  useEffect(() => {
-    const getCategoryData = async () => {
-        const respCategories = await axios.get(`https://www.themealdb.com/api/json/v1/1/categories.php`)
-        setCategories(respCategories.data.categories);
     }
-    getCategoryData();
-    
-}, []);
-   useEffect(() => {
+
+    useEffect(() => {
+        const getCategoryData = async () => {
+            const respCategories = await axios.get(`https://www.themealdb.com/api/json/v1/1/categories.php`)
+            setCategories(respCategories.data.categories);
+        }
+        getCategoryData();
+
+    }, []);
+    useEffect(() => {
         let newPagination = [];
         for (let i = 0; i < filtered.length / 8; i++) {
             newPagination.push(
@@ -56,41 +57,43 @@ const MenuHeader = () => {
         setPagination(newPagination);
     }, []);
 
-  return (
-    <div class="meals-container">
-        <HeaderYourMenu />
-        <div className='categories'>
+    return (
+        <div class="meals-container">
+            <HeaderYourMenu />
+            <div className='categories'>
                 {categories.map(category => (
-                    <button onClick={ ()=> handleOnClickItem(category.strCategory)} className='btn-category'>
+                    <button onClick={() => handleOnClickItem(category.strCategory)} className='btn-category'>
                         <div className='category' key={category.idCategory}>
-                        <img width='60px' src={category.strCategoryThumb} alt="Not exist" />
-                        <h6>{category.strCategory}</h6>
-                    </div>
+                            <img width='60px' src={category.strCategoryThumb} alt="Not exist" />
+                            <h6>{category.strCategory}</h6>
+                        </div>
                     </button>
                 ))}
-        </div>
-        <div className='meals'>
-            <button className="button-scroll" onClick={() => (page === 0 ? null : setPage(page - 1))}><ion-icon className='icon' name="chevron-back-outline"></ion-icon></button>
-                    {filtered.slice(page * 4, (page + 1) * 4).map((key) => 
-                            <div className="cardmeal1">
-                                <Heart recip={filtered} select={select} setSelect={setSelect} id={key.idMeal} meal={key.strMeal} img={key.strMealThumb}/>
-                                <CardMedia className="img1" key={key.idMeal} 
-                                    component="img"
-                                    src={key.strMealThumb}
-                                    alt={key.strMeal}
-                                />
-                                <div className="content-text1">
-                                    <h4 className="title-meal1">{key.strMeal}</h4>
-                                </div>
+            </div>
+            <div className='meals'>
+                <button className="button-scroll" onClick={() => (page === 0 ? null : setPage(page - 1))}><ion-icon className='icon' name="chevron-back-outline"></ion-icon></button>
+                {filtered.slice(page * 4, (page + 1) * 4).map((key) =>
+                    <div className="cardmeal1">
+                        <CardMedia className="img1" key={key.idMeal}
+                            component="img"
+                            src={key.strMealThumb}
+                            alt={key.strMeal}
+                        />
+                        <div className="content-text1">
+                            <div className="title-container1">
+                            <h4 className="title-meal1">{key.strMeal}</h4>
                             </div>
-                    )}
-                    <button id='btn-right' className="button-scroll" onClick={() => (page === 7 ? null : setPage(page + 1))}><ion-icon className='icon' name="chevron-forward-outline"></ion-icon></button>
+                            <Heart recip={filtered} select={select} setSelect={setSelect} id={key.idMeal} meal={key.strMeal} img={key.strMealThumb} />
+                        </div>
+                    </div>
+                )}
+                <button id='btn-right' className="button-scroll" onClick={() => (page === 7 ? null : setPage(page + 1))}><ion-icon className='icon' name="chevron-forward-outline"></ion-icon></button>
+            </div>
+            <h2 className="sentence">Recipes from your special menu </h2>
+            <MealsMenu recip={select} setSelect={setSelect} />
         </div>
-        <h1>Recipes from your special menu </h1>
-        <MealsMenu recip={select} setSelect={setSelect}/>
-  </div>
-)
-} 
-  
+    )
+}
+
 
 export default MenuHeader
